0ffe9ac8fd226ae7a8effe136bfb94775b0bceb8
benjaminherrington14 <benjamin.herrington14@gmail.com>
2025-10-09 18:53:39 -0400

    Finished Parser Testing

--- backend/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
+++ backend/TestProject/obj/Debug/net9.0/TestProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca05fdf96f82e738608a78c7bda4b8daf54f5e3b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+005c2797c6b61aad4461a520c81bd9d1d77be555")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
