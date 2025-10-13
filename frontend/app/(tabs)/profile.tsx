c079bbf704f93beaa7d5bf4e98fecd2a2031c73e
KatLowe51 <lowe116@purdue.edu>
2025-10-13 13:58:46 -0400

    profile page book sections updated

--- frontend/app/(tabs)/profile.tsx
+++ frontend/app/(tabs)/profile.tsx
@@ -1,9 +1,18 @@
 ﻿import React from 'react';
 import StyleSheet from '../../styles/globalStyles';
 import Foundation from '@expo/vector-icons/Foundation'; //imported from react
-
-//here is me playing around with shit that I don't understand
-
+import { Platform, StyleSheet } from 'react-native';
+import ParallaxScrollView from '../../components/parallax-scroll-view';
+import { ThemedText } from '../../components/themed-text';
+import { ThemedView } from '../../components/themed-view';
+import { Link } from 'expo-router';
+import { View } from 'react-native';
+import { Image } from 'expo-image';
+//import ProfilePic from '@../../assets/images/userprofile_icon.png';
+import SearchBar from '@/components/SearchBar';
+import Header from '../headerWithSearch';
+import { StyleSheet } from 'react-native';
+import { colors, fonts } from '../theme';
 
 
 function EditButton() {
@@ -14,6 +23,116 @@ function EditButton() {
         </button>
     );
 }
+
+type Books = { //allow the books to be displayed on the screen
+    title: string;
+    coverUrl: string;
+}
+
+type SectionProps = { //a section has a title ans list of books in that section
+    name: string;
+    list: Books[];
+}
+
+function BookSection({name, list}: SectionProps) {
+    return (
+        <View style={styles}>
+            <Heading> {name}</Heading>
+            //implement scrollable menu like https://www.w3schools.com/howto/howto_css_menu_horizontal_scroll.asp in CSS
+                for(let i = 0; i < list.length; i++) {
+                    <View style={styles.scrollingWrapper}>
+            <ScrollView style={styles.scrollContainer} horizontal={true} showsHorizontalScrollIndicator={true}>
+                {/* cards that scroll horizontally */}
+                <View style={styles.card}>
+                    <Text>{list[i].name}</Text>
+                    //add the cover image
+                </View>
+            } //exit javascript mode
+        </View>
+    );
+}
+
+
+
+
+
+
+//vidya's header and search bar
+//profile photo left aligned
+//big text fitstname last name
+//smaller text @username
+//normal small text bio
+//divider
+//top three centered heading
+//three books
+//currently reading centered heading
+//iew all currently reading
+//divider
+//past reads centered heading
+//scroll left right past reads
+//divider
+//saved for later centered heading
+//scroll left right saved for later
+//divider
+//your LitClubs
+//footer with the default buttons on it
+
+
+
+//initial set up code borrowed from expo router template
+// https://docs.expo.dev/tutorial/create-your-first-app/
+
+
+
+export default function ProfileScreen() {
+    return (
+        // TO DO: insert header at the top, which will include the logo and search bar
+        <View>
+         <Header />
+        <ParallaxScrollView>
+            <ThemedView style={styles.container}>
+                <View style={styles.imageContainer}>
+                    <Image
+                        source={require('@/assets/images/userprofile_icon.png')}
+                        style={styles.image}
+                    />
+                </View>
+                <heading type="title">Firstname Lastname</heading>
+                <ThemedText type="subtitle">@username</ThemedText>
+                <EditButton />
+                
+            </ThemedView>
+        </ParallaxScrollView>
+        </View>
+    );
+}
+
+
+<BookSection
+name="Your Top Three"
+list={{"Six of Crows","https://upload.wikimedia.org/wikipedia/en/1/10/Six_of_Crows_by_Leigh_Bardugo_book_cover.jpeg"}, 
+{"Atonement", "https://upload.wikimedia.org/wikipedia/en/6/6d/Atonement_%28novel%29.jpg"},
+{"The Fault in Our Stars","https://upload.wikimedia.org/wikipedia/en/a/a9/The_Fault_in_Our_Stars.jpg" }   }
+/>
+
+<BookSection
+name="Currently Reading"
+list={{"Gideon the Ninth", "https://m.media-amazon.com/images/I/81UgIZXiolL._UF1000,1000_QL80_.jpg"} 
+}
+/>
+
+<BookSection
+name="Past Reads"
+list={{}
+}
+/>
+
+<BookSection
+name="Saved for Later"
+list{{}
+}
+/>
+
 
 //overall profile page
 export default function profile() {
@@ -33,98 +152,7 @@ export default function profile() {
     );
 }
 
-type Books = { //allow the books to be displayed on the screen
-    title: string;
-    coverUrl: string;
-}
 
-type SectionProps = { //a section has a title ans list of books in that section
-    name: string;
-    list: Books[];
-}
-
-function BookSection({name, list}: SectionProps) {
-    return (
-        <View style={styles}>
-            <Heading> {name}</Heading>
-            //implement scrollable menu like https://www.w3schools.com/howto/howto_css_menu_horizontal_scroll.asp in CSS
-            {
-                for(let i = 0; i < list.length; i++) {
-                    <View style={}
-            }
-                book1
-                book2
-                book3
-            </p>
-
-        </View>
-    );
-}
-
-
-
-
-//vidya's header and search bar
-//profile photo left aligned
-//big text fitstname last name
-//smaller text @username
-//normal small text bio
-//divider
-//top three centered heading
-//three books
-//currently reading centered heading
-//iew all currently reading
-//divider
-//past reacds centered heading
-//scroll left right past reads
-//divider
-//saved for later centered heading
-//scroll left right saved for later
-//divider
-//your LitClubs
-//footer with the default buttons on it
-
-
-
-//initial set up code borrowed from expo router template
-// https://docs.expo.dev/tutorial/create-your-first-app/
-
-import { Platform, StyleSheet } from 'react-native';
-
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { Link } from 'expo-router';
-import { View } from 'react-native';
-import { Image } from 'expo-image';
-//import ProfilePic from '@../../assets/images/userprofile_icon.png';
-import SearchBar from '@/components/SearchBar';
-import Header from '../headerWithSearch';
-
-export default function ProfileScreen() {
-    return (
-        // TO DO: insert header at the top, which will include the logo and search bar
-        <View>
-         <Header />
-        <ParallaxScrollView>
-            <ThemedView style={styles.container}>
-                <View style={styles.imageContainer}>
-                    <Image
-                        source={require('@/assets/images/userprofile_icon.png')}
-                        style={styles.image}
-                    />
-                </View>
-                <ThemedText type="title">Firstname Lastname</ThemedText>
-                <ThemedText type="subtitle">@username</ThemedText>
-                
-            </ThemedView>
-        </ParallaxScrollView>
-        </View>
-    );
-}
-
-import { StyleSheet } from 'react-native';
-import { colors, fonts } from '../theme';
 
 export const globalStyles = StyleSheet.create({
   container: {
@@ -150,4 +178,20 @@ export const globalStyles = StyleSheet.create({
     color: colors.darkest,
     lineHeight: 22,
   },
+   scrollContainer: {
+        overflowX: 'scroll',
+        overflowY: 'hidden',
+        whiteSpace: 'nowrap',
+        padding: 10,
+    },
+    scrollingWrapper: {
+        flex: 1,
+    },
+    card: {
+        width: 120,
+        height: 180,
+        marginRight: 10,
+        backgroundColor: "teal",
+        borderColor: "black",
+    },
 });
