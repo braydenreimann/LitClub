5696545e06c55625cfeb171b4ab70bbaa19d2156
Brayden Reimann <braydenreimann@icloud.com>
2025-10-13 09:58:38 -0400

    Changed some packages, commented out file that is breaking testing

--- frontend/app/(tabs)/testing_themesheet.tsx
+++ frontend/app/(tabs)/testing_themesheet.tsx
@@ -1,41 +1,41 @@
-import { ChivoMono_500Medium } from '@expo-google-fonts/chivo-mono';
-import { Fraunces_700Bold, useFonts } from '@expo-google-fonts/fraunces';
-import { NotoSansMono_400Regular } from '@expo-google-fonts/noto-sans-mono';
-import * as SplashScreen from 'expo-splash-screen';
-import React from 'react';
-import { View } from 'react-native';
-import BodyText from '../../components/BodyText';
-import Heading from '../../components/Heading';
-import Subheading from '../../components/Subheading';
-import { globalStyles } from '../../styles/globalStyles';
-import { colors } from '../../theme';
+// import { ChivoMono_500Medium } from '@expo-google-fonts/chivo-mono';
+// import { Fraunces_700Bold, useFonts } from '@expo-google-fonts/fraunces';
+// import { NotoSansMono_400Regular } from '@expo-google-fonts/noto-sans-mono';
+// import * as SplashScreen from 'expo-splash-screen';
+// import React from 'react';
+// import { View } from 'react-native';
+// import BodyText from '../../components/BodyText';
+// import Heading from '../../components/Heading';
+// import Subheading from '../../components/Subheading';
+// import { globalStyles } from '../../styles/globalStyles';
+// import { colors } from '../../theme';
 
 
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
-export default function testing_themesheet() {
-  const [fontsLoaded] = useFonts({
-    Fraunces_700Bold,
-    ChivoMono_500Medium,
-    NotoSansMono_400Regular,
-  });
+// export default function testing_themesheet() {
+//   const [fontsLoaded] = useFonts({
+//     Fraunces_700Bold,
+//     ChivoMono_500Medium,
+//     NotoSansMono_400Regular,
+//   });
 
-  React.useEffect(() => {
-    if (fontsLoaded) SplashScreen.hideAsync();
-  }, [fontsLoaded]);
+//   React.useEffect(() => {
+//     if (fontsLoaded) SplashScreen.hideAsync();
+//   }, [fontsLoaded]);
 
-  if (!fontsLoaded) return null;
+//   if (!fontsLoaded) return null;
 
-  return (
-    <View style={globalStyles.container}>
-      <Heading style={{ color: colors.midBlue}}>Example of Heading</Heading>
-      <Subheading style={{ color: colors.sage }}>Example of Subheading</Subheading>
-      <Subheading>If you don't specify style, this is the default subheading color!</Subheading>
+//   return (
+//     <View style={globalStyles.container}>
+//       <Heading style={{ color: colors.midBlue}}>Example of Heading</Heading>
+//       <Subheading style={{ color: colors.sage }}>Example of Subheading</Subheading>
+//       <Subheading>If you don't specify style, this is the default subheading color!</Subheading>
 
-      <BodyText>
-        This body text automatically uses your Noto Sans Mono font and color scheme.
-        Isn't it great? I love body text.
-      </BodyText>
-    </View>
-  );
-}
+//       <BodyText>
+//         This body text automatically uses your Noto Sans Mono font and color scheme.
+//         Isn't it great? I love body text.
+//       </BodyText>
+//     </View>
+//   );
+// }
