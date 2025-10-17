58c7fe2690132ee68f8619422335a66fff7ab641
benjaminherrington14 <benjamin.herrington14@gmail.com>
2025-10-17 00:38:19 -0400

    Added getBookShelf

--- frontend/profile/profileService.tsx
+++ frontend/profile/profileService.tsx
@@ -24,6 +24,21 @@ export interface User {
     created: string; // ISO date string, e.g. "2025-10-17T00:37:46.126Z"
 }
 
+export interface LibraryBook {
+    id: string;
+    status: number;               
+    startedReading: string;       
+    finishedReading: string;      
+    currentPage: number;
+    percentComplete: number;
+    onPedastal: boolean;
+}
+
+export interface DisplayBook {
+    id: number;
+    title: string;
+}
+
 export async function getUser(): Promise<User | null> {
     try {
         const sessionString = await AsyncStorage.getItem('session');
@@ -34,5 +49,33 @@ export async function getUser(): Promise<User | null> {
     } catch (error) {
         console.error('Error retrieving user from session:', error);
         return null;
+    }
+}
+
+export async function getBookshelf(userId: string, status: number): Promise<DisplayBook[] | null> {
+    try {
+        const response = await fetch(`${API_BASE_URL}/libraries/${userId}/libraryBooks`);
+        if(!response.ok) {
+                console.warn('Failed to fetch bookshelf', response.status);
+        return [];
+        }
+
+        const unsortedbooks: LibraryBook[] = await response.json();
+        let sortedbooks: DisplayBook[] = [];
+        let i: number = 1;
+        
+        unsortedbooks.forEach((book) => {
+            if (book.status === status) {
+                const bookToAdd: DisplayBook = { id: i, title: book.id };
+                i++;
+                sortedbooks.push(bookToAdd);
+            }
+        });
+
+        return sortedbooks;
+
+    } catch (error) {
+      console.error('Error fetching bookshelf:', error);
+      return null;
     }
 }
