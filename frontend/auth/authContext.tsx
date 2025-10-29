5b960d178d8265d779f3c7846dc6705151787715
Brayden Reimann <braydenreimann@icloud.com>
2025-10-29 18:20:03 -0400

    Refactored to use mappers and new input objects

--- frontend/auth/authContext.tsx
+++ frontend/auth/authContext.tsx
@@ -1,29 +1,19 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import React, { createContext, useContext, useEffect, useState, type PropsWithChildren } from 'react';
 import { client } from 'client';
-import { components } from 'schema/openapi-types';
 
-type LoginRequest = components["schemas"]["LoginRequest"];
-type SignUpRequest = components["schemas"]["CreateAccountRequest"]
-
-export interface CreateAccountPayload {
-  firstName: string;
-  lastName: string;
-  userName: string;
-  email: string;
-  password: string;
-  bio?: string;
-  profilePhotoUrl?: string | null;
-  preferredGenres?: string[] | null;
-  privateAccount: boolean;
-  publicInteractionRestricted: boolean;
-}
+import {
+  toLoginRequest,
+  toCreateAccountRequest,
+  type LoginInput,
+  type CreateAccountInput
+} from '../api-mappers/auth/auth-mappers';
 
 // ---- Context types
 type AuthContextType = {
-  signIn: (email: string, password: string) => Promise<boolean>;
+  signIn: (input: LoginInput) => Promise<boolean>;
   signOut: () => Promise<boolean>;
-  register: (payload: CreateAccountPayload) => Promise<boolean>;
+  register: (payload: CreateAccountInput) => Promise<boolean>;
   session: string | null;
   isLoading: boolean;
 };
@@ -49,80 +39,44 @@ export function SessionProvider({ children }: PropsWithChildren) {
     loadSession();
   }, []);
 
-  const signIn = async (email: string, password: string): Promise<boolean> => {
+  const signIn = async (input: LoginInput): Promise<boolean> => {
     try {
-      const loginRequest: LoginRequest =
-      {
-        userName: null,
-        email: email,
-        password: password
-      }
-
-      const { data, error } = await client.POST("/users/login",
-        {
-          body: loginRequest
-        }
-      );
+      const body = toLoginRequest(input);
+      const { data, error } = await client.POST("/users/login", { body });
 
       if (error) {
-        // Handle login error
+        console.warn(`Login failed. ${error.detail}`);
+        return false;
       }
 
-      // if (!response.ok) {
-      //   const msg = await response.text().catch(() => '');
-      //   console.warn('Login failed', response.status, msg);
-      //   return false;
-      // }
-
       const sessionData = JSON.stringify(data);
-      console.error(sessionData);
-
       setSession(sessionData);
       await AsyncStorage.setItem('session', sessionData);
+
       return true;
-    } catch (err) {
-      console.error('Sign-in failed', err);
+    } catch (e) {
+      console.error('Sign-in failed unexpectedly', e);
       return false;
     }
   };
 
-  const register = async (payload: CreateAccountPayload): Promise<boolean> => {
+  const register = async (input: CreateAccountInput): Promise<boolean> => {
     try {
-      const signUpRequest: SignUpRequest =
-      {
-        firstName: payload.firstName,
-        lastName: payload.lastName,
-        userName: payload.userName,
-        email: payload.email,
-        password: payload.password,
-        bio: payload.bio,
-        preferredGenres: payload.preferredGenres,
-        privateAccount: payload.privateAccount,
-        publicInteractionRestricted: payload.publicInteractionRestricted,
-      }
-
-      const { data, error } = await client.POST("/users/register",
-        {
-          body: signUpRequest
-        }
-      );
+      const body = toCreateAccountRequest(input);
+      const { data, error } = await client.POST("/users/register", { body });
 
       if (error) {
-        // Handle login error 
+        if (error.status === 409) {
+          console.warn('Username or email already exists.');
+          return false;
+        }
+
+        console.warn(`Login failed. ${error.detail}`);
+        return false;
       }
 
-      // if (res.status === 409) {
-      //   console.warn('Username or email already exists.');
-      //   return false;
-      // }
-      // if (!res.ok) {
-      //   const msg = await res.text().catch(() => '');
-      //   console.warn('Register failed', res.status, msg);
-      //   return false;
-      // }
-
       const serialized = JSON.stringify(data);
-      setSession(serialized); // auto-login for demo
+      setSession(serialized);
       await AsyncStorage.setItem('session', serialized);
       return true;
     } catch (err) {
