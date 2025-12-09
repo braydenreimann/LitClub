883510a918703f6e38a53e86ad24bbdc8f6d1a80
Brayden Reimann <braydenreimann@icloud.com>
2025-12-09 16:18:03 -0500

    Removed unused endpoints (less maintenance!)

--- backend/LitClubApi/Endpoints/Libraries/ListLibraries/ListLibrariesResponse.cs
+++
@@ -1,8 +0,0 @@
-﻿// namespace LitClubApi.Endpoints.Libraries.ListLibraries
-// {
-//     public sealed class ListLibrariesResponse
-//     {
-//         public required List<LibraryResponse> Libraries { get; init; } = [];
-//         public string? ContinuationToken { get; init; }
-//     }
-// }
